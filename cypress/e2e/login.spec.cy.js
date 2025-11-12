aa68be26ff7a817a25666980b54318a29d9a16d8
João Freire da Silva Neto <freireneto92@gmail.com>
2025-11-12 00:08:44 -0300

    add objeto selectorList

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,17 @@
 describe('Orange HRM Test spec', () => {
+
+      const selectorList = {
+
+        checkUserRoleList: ".oxd-select-wrapper",
+        checkStatusList: ".oxd-select-wrapper",
+        selectRoleList: ".oxd-select-dropdown",
+        selectStatusList: ".oxd-select-dropdown"
+
+
+
+      }
+
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('[name="username"]').type('Admin')
@@ -8,10 +21,10 @@ describe('Orange HRM Test spec', () => {
     cy.contains('span', 'Admin', {timeout: 10000}).click()
     cy.get('[data-v-957b4417] .oxd-input' ).type('JoãoQA')
     cy.get('input[placeholder="Type for hints..."]', { timeout: 10000 }).type('freire')
-    cy.get('.oxd-select-wrapper').eq(0).click()
-    cy.contains('.oxd-select-dropdown' , 'Admin').click()
-    cy.get('.oxd-select-wrapper').eq(1).click()
-    cy.contains('.oxd-select-dropdown', 'Enable').click()
+    cy.get(selectorList.checkUserRoleList).eq(0).click()
+    cy.contains(selectorList.selectRoleList, 'Admin').click()
+    cy.get(selectorList.checkStatusList).eq(1).click()
+    cy.contains(selectorList.selectStatusList, 'Enable').click()
     cy.contains('button' , 'Reset').click()
   })
 })
