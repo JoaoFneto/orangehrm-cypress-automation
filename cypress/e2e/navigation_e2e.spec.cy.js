5e0d0cd30a9e0bff120223f24c99ebede8517138
João Freire da Silva Neto <freireneto92@gmail.com>
2025-12-06 23:05:16 -0300

    Added Object Myinfo

--- cypress/e2e/navigation_e2e.spec.cy.js
+++ cypress/e2e/navigation_e2e.spec.cy.js
@@ -7,9 +7,16 @@ describe('Orange HRM Test spec', () => {
     selectRoleList: ".oxd-select-dropdown",
     selectStatusList: ".oxd-select-dropdown",
     dashbordGrid: ".orangehrm-dashboard-grid",
-    buttonMyinfo:'[href="/web/index.php/pim/viewMyDetails"]'
-  }
+    buttonMyinfo:'[href="/web/index.php/pim/viewMyDetails"]',
+    myinfoFirstName:".oxd-input--active",
+    myinfoMiddleName:".orangehrm-middlename",
+    myinfoLastName:".oxd-input--active",
+    myinfoIdFunc:".oxd-input--active",
+    myinfoOutherId:".oxd-input--active",
+    myinfoDriveId:".oxd-input--active",
+    myinfoLicense:".oxd-icon bi-calendar"
 
+  }
 
   it('Login - Success', () => {
     cy.visit('/auth/login')
@@ -17,7 +24,7 @@ describe('Orange HRM Test spec', () => {
     cy.get('[name="password"]').type('admin123')
     cy.contains('button', ' Login ').click()
     cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashbordGrid)
+    cy.get(selectorList.dashbordGrid,{timeout:1000})
     cy.contains('span', 'Admin', { timeout: 10000 }).click()
     cy.get('[data-v-957b4417] .oxd-input').type('JoãoQA')
     cy.get('input[placeholder="Type for hints..."]', { timeout: 10000 }).type('freire')
@@ -27,5 +34,15 @@ describe('Orange HRM Test spec', () => {
     cy.contains(selectorList.selectStatusList, 'Enable').click()
     cy.contains('button', 'Reset').click()
     cy.get(selectorList.buttonMyinfo).click()
+    cy.get(selectorList.myinfoFirstName).eq(1).clear().type('João')
+    cy.get(selectorList.myinfoMiddleName).clear().type('Freire')
+    cy.get(selectorList.myinfoLastName).eq(2).clear().type('Silva')
+    cy.get(selectorList.myinfoIdFunc).eq(3).clear().type('1022')
+    cy.get(selectorList.myinfoOutherId,{timeout:10000}).eq(4).clear().type('22')
+    cy.get(selectorList.myinfoDriveId).eq(5).clear().type('856431')
+    cy.get('[data-v-4a95a2e0] ,.oxd-date-wrapper').eq(0).click()
+    cy.contains('[data-v-4a95a2e0] ,.oxd-date-wrapper', 'Clear').click()
+       .type('2025-12-12')
+
   })
 })
